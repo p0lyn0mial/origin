vrutkovs/kubernetes

UPSTREAM: <carry>: adapt to the new flag 'structure'
Origin-commit: 7359416d385e176ba22ebaf55ed81422f193395f

test/e2e/lifecycle/patch_flagaccess.go
@@ -1,0 +1,17 @@
+package lifecycle
+
+func GetUpgradeTarget() string {
+	return *upgradeTarget
+}
+
+func SetUpgradeTarget(val string) {
+	upgradeTarget = &val
+}
+
+func GetUpgradeImage() string {
+	return *upgradeImage
+}
+
+func SetUpgradeImage(val string) {
+	upgradeImage = &val
+}

p0lyn0mial/origin

test/e2e/cloud/gcp/patch_flagaccess.go
@@ -1,0 +1,17 @@
+package gcp
+
+func GetUpgradeTarget() string {
+	return *upgradeTarget
+}
+
+func SetUpgradeTarget(val string) {
+	upgradeTarget = &val
+}
+
+func GetUpgradeImage() string {
+	return *upgradeImage
+}
+
+func SetUpgradeImage(val string) {
+	upgradeImage = &val
+}