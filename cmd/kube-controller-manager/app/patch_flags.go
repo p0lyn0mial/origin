vrutkovs/kubernetes

UPSTREAM: <carry>: kube-controller-manager: allow running bare kube-controller-manager

cmd/kube-controller-manager/app/patch_flags.go
@@ -1,0 +1,124 @@
+package app
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	kyaml "k8s.io/apimachinery/pkg/util/yaml"
+	apiserverflag "k8s.io/apiserver/pkg/util/flag"
+	"k8s.io/kubernetes/cmd/kube-controller-manager/app/config"
+	"k8s.io/kubernetes/cmd/kube-controller-manager/app/options"
+)
+
+func getOpenShiftConfig(configFile string) (map[string]interface{}, error) {
+	configBytes, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return nil, err
+	}
+	jsonBytes, err := kyaml.ToJSON(configBytes)
+	if err != nil {
+		return nil, err
+	}
+	config := map[string]interface{}{}
+	if err := json.Unmarshal(jsonBytes, &config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func applyOpenShiftConfigFlags(controllerManagerOptions *options.KubeControllerManagerOptions, controllerManager *config.Config, openshiftConfig map[string]interface{}) error {
+	if err := applyOpenShiftConfigControllerArgs(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigDefaultProjectSelector(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigKubeDefaultProjectSelector(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	return controllerManagerOptions.ApplyTo(controllerManager)
+}
+
+func applyOpenShiftConfigDefaultProjectSelector(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	projectConfig, ok := openshiftConfig["projectConfig"]
+	if !ok {
+		return nil
+	}
+
+	castProjectConfig := projectConfig.(map[string]interface{})
+	defaultNodeSelector, ok := castProjectConfig["defaultNodeSelector"]
+	if !ok {
+		return nil
+	}
+	controllerManagerOptions.OpenShiftContext.OpenShiftDefaultProjectNodeSelector = defaultNodeSelector.(string)
+
+	return nil
+}
+
+// this is an optimization.  It can be filled in later.  Looks like there are several special cases for this plugin upstream
+// TODO find this
+func applyOpenShiftConfigKubeDefaultProjectSelector(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	controllerManagerOptions.OpenShiftContext.KubeDefaultProjectNodeSelector = ""
+	return nil
+}
+
+func applyOpenShiftConfigControllerArgs(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	var controllerArgs interface{}
+	kubeMasterConfig, ok := openshiftConfig["kubernetesMasterConfig"]
+	if !ok {
+		controllerArgs, ok = openshiftConfig["extendedArguments"]
+		if !ok || controllerArgs == nil {
+			return nil
+		}
+	} else {
+		castKubeMasterConfig := kubeMasterConfig.(map[string]interface{})
+		controllerArgs, ok = castKubeMasterConfig["controllerArguments"]
+		if !ok || controllerArgs == nil {
+			controllerArgs, ok = openshiftConfig["extendedArguments"]
+			if !ok || controllerArgs == nil {
+				return nil
+			}
+		}
+	}
+
+	args := map[string][]string{}
+	for key, value := range controllerArgs.(map[string]interface{}) {
+		for _, arrayValue := range value.([]interface{}) {
+			args[key] = append(args[key], arrayValue.(string))
+		}
+	}
+	if err := applyFlags(args, controllerManagerOptions.Flags(KnownControllers(), ControllersDisabledByDefault.List())); len(err) > 0 {
+		return kerrors.NewAggregate(err)
+	}
+	return nil
+}
+
+// applyFlags stores the provided arguments onto a flag set, reporting any errors
+// encountered during the process.
+func applyFlags(args map[string][]string, flags apiserverflag.NamedFlagSets) []error {
+	var errs []error
+	for key, value := range args {
+		found := false
+		// since flags are now groupped into sets we need to check each and every
+		// single of them and complain only if we don't find the flag at all
+		for _, fs := range flags.FlagSets {
+			if flag := fs.Lookup(key); flag != nil {
+				for _, s := range value {
+					if err := flag.Value.Set(s); err != nil {
+						errs = append(errs, field.Invalid(field.NewPath(key), s, fmt.Sprintf("could not be set: %v", err)))
+						break
+					}
+				}
+				found = true
+			}
+		}
+		if !found {
+			errs = append(errs, field.Invalid(field.NewPath("flag"), key, "is not a valid flag"))
+		}
+	}
+	return errs
+}

cmd/kube-controller-manager/app/patch_gc.go
@@ -1,0 +1,45 @@
+package app
+
+import (
+	"k8s.io/kubernetes/cmd/kube-controller-manager/app/config"
+	kubectrlmgrconfig "k8s.io/kubernetes/pkg/controller/apis/config"
+)
+
+func applyOpenShiftGCConfig(controllerManager *config.Config) error {
+	// TODO make this configurable or discoverable.  This is going to prevent us from running the stock GC controller
+	// IF YOU ADD ANYTHING TO THIS LIST, MAKE SURE THAT YOU UPDATE THEIR STRATEGIES TO PREVENT GC FINALIZERS
+	controllerManager.ComponentConfig.GarbageCollectorController.GCIgnoredResources = append(controllerManager.ComponentConfig.GarbageCollectorController.GCIgnoredResources,
+		// explicitly disabled from GC for now - not enough value to track them
+		kubectrlmgrconfig.GroupResource{Group: "authorization.openshift.io", Resource: "rolebindingrestrictions"},
+		kubectrlmgrconfig.GroupResource{Group: "network.openshift.io", Resource: "clusternetworks"},
+		kubectrlmgrconfig.GroupResource{Group: "network.openshift.io", Resource: "egressnetworkpolicies"},
+		kubectrlmgrconfig.GroupResource{Group: "network.openshift.io", Resource: "hostsubnets"},
+		kubectrlmgrconfig.GroupResource{Group: "network.openshift.io", Resource: "netnamespaces"},
+		kubectrlmgrconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthclientauthorizations"},
+		kubectrlmgrconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthclients"},
+		kubectrlmgrconfig.GroupResource{Group: "quota.openshift.io", Resource: "clusterresourcequotas"},
+		kubectrlmgrconfig.GroupResource{Group: "user.openshift.io", Resource: "groups"},
+		kubectrlmgrconfig.GroupResource{Group: "user.openshift.io", Resource: "identities"},
+		kubectrlmgrconfig.GroupResource{Group: "user.openshift.io", Resource: "users"},
+		kubectrlmgrconfig.GroupResource{Group: "image.openshift.io", Resource: "images"},
+
+		// virtual resource
+		kubectrlmgrconfig.GroupResource{Group: "project.openshift.io", Resource: "projects"},
+		// virtual and unwatchable resource, surfaced via rbac.authorization.k8s.io objects
+		kubectrlmgrconfig.GroupResource{Group: "authorization.openshift.io", Resource: "clusterroles"},
+		kubectrlmgrconfig.GroupResource{Group: "authorization.openshift.io", Resource: "clusterrolebindings"},
+		kubectrlmgrconfig.GroupResource{Group: "authorization.openshift.io", Resource: "roles"},
+		kubectrlmgrconfig.GroupResource{Group: "authorization.openshift.io", Resource: "rolebindings"},
+		// these resources contain security information in their names, and we don't need to track them
+		kubectrlmgrconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthaccesstokens"},
+		kubectrlmgrconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthauthorizetokens"},
+		// exposed already as extensions v1beta1 by other controllers
+		kubectrlmgrconfig.GroupResource{Group: "apps", Resource: "deployments"},
+		// exposed as autoscaling v1
+		kubectrlmgrconfig.GroupResource{Group: "extensions", Resource: "horizontalpodautoscalers"},
+		// exposed as security.openshift.io v1
+		kubectrlmgrconfig.GroupResource{Group: "", Resource: "securitycontextconstraints"},
+	)
+
+	return nil
+}

pkg/controller/daemon/daemon_controller.go
@@ -126,7 +126,13 @@ type DaemonSetsController struct {
 	nodeLister corelisters.NodeLister
 	// nodeStoreSynced returns true if the node store has been synced at least once.
 	// Added as a member to the struct to allow injection for testing.
-	nodeStoreSynced cache.InformerSynced
+	nodeStoreSynced                    cache.InformerSynced
+	namespaceLister                    corelisters.NamespaceLister
+	namespaceStoreSynced               cache.InformerSynced
+	openshiftDefaultNodeSelectorString string
+	openshiftDefaultNodeSelector       labels.Selector
+	kubeDefaultNodeSelectorString      string
+	kubeDefaultNodeSelector            labels.Selector
 
 	// DaemonSet keys that need to be synced.
 	queue workqueue.RateLimitingInterface
@@ -271,6 +277,11 @@ func (dsc *DaemonSetsController) Run(workers int, stopCh <-chan struct{}) {
 
 	if !controller.WaitForCacheSync("daemon sets", stopCh, dsc.podStoreSynced, dsc.nodeStoreSynced, dsc.historyStoreSynced, dsc.dsStoreSynced) {
 		return
+	}
+	if dsc.namespaceStoreSynced != nil {
+		if !controller.WaitForCacheSync("daemon sets", stopCh, dsc.namespaceStoreSynced) {
+			return
+		}
 	}
 
 	for i := 0; i < workers; i++ {
@@ -1410,6 +1421,14 @@ func (dsc *DaemonSetsController) nodeShouldRunDaemonPod(node *v1.Node, ds *apps.
 			}
 		}
 	}
+
+	if matches, matchErr := dsc.namespaceNodeSelectorMatches(node, ds); matchErr != nil {
+		return false, false, false, matchErr
+	} else if !matches {
+		shouldSchedule = false
+		shouldContinueRunning = false
+	}
+
 	// only emit this event if insufficient resource is the only thing
 	// preventing the daemon pod from scheduling
 	if shouldSchedule && insufficientResourceErr != nil {

p0lyn0mial/origin

cmd/kube-controller-manager/app/patch_flags.go
@@ -1,0 +1,124 @@
+package app
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	kyaml "k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/component-base/cli/flag"
+	"k8s.io/kubernetes/cmd/kube-controller-manager/app/config"
+	"k8s.io/kubernetes/cmd/kube-controller-manager/app/options"
+)
+
+func getOpenShiftConfig(configFile string) (map[string]interface{}, error) {
+	configBytes, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return nil, err
+	}
+	jsonBytes, err := kyaml.ToJSON(configBytes)
+	if err != nil {
+		return nil, err
+	}
+	config := map[string]interface{}{}
+	if err := json.Unmarshal(jsonBytes, &config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func applyOpenShiftConfigFlags(controllerManagerOptions *options.KubeControllerManagerOptions, controllerManager *config.Config, openshiftConfig map[string]interface{}) error {
+	if err := applyOpenShiftConfigControllerArgs(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigDefaultProjectSelector(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigKubeDefaultProjectSelector(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	return controllerManagerOptions.ApplyTo(controllerManager)
+}
+
+func applyOpenShiftConfigDefaultProjectSelector(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	projectConfig, ok := openshiftConfig["projectConfig"]
+	if !ok {
+		return nil
+	}
+
+	castProjectConfig := projectConfig.(map[string]interface{})
+	defaultNodeSelector, ok := castProjectConfig["defaultNodeSelector"]
+	if !ok {
+		return nil
+	}
+	controllerManagerOptions.OpenShiftContext.OpenShiftDefaultProjectNodeSelector = defaultNodeSelector.(string)
+
+	return nil
+}
+
+// this is an optimization.  It can be filled in later.  Looks like there are several special cases for this plugin upstream
+// TODO find this
+func applyOpenShiftConfigKubeDefaultProjectSelector(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	controllerManagerOptions.OpenShiftContext.KubeDefaultProjectNodeSelector = ""
+	return nil
+}
+
+func applyOpenShiftConfigControllerArgs(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	var controllerArgs interface{}
+	kubeMasterConfig, ok := openshiftConfig["kubernetesMasterConfig"]
+	if !ok {
+		controllerArgs, ok = openshiftConfig["extendedArguments"]
+		if !ok || controllerArgs == nil {
+			return nil
+		}
+	} else {
+		castKubeMasterConfig := kubeMasterConfig.(map[string]interface{})
+		controllerArgs, ok = castKubeMasterConfig["controllerArguments"]
+		if !ok || controllerArgs == nil {
+			controllerArgs, ok = openshiftConfig["extendedArguments"]
+			if !ok || controllerArgs == nil {
+				return nil
+			}
+		}
+	}
+
+	args := map[string][]string{}
+	for key, value := range controllerArgs.(map[string]interface{}) {
+		for _, arrayValue := range value.([]interface{}) {
+			args[key] = append(args[key], arrayValue.(string))
+		}
+	}
+	if err := applyFlags(args, controllerManagerOptions.Flags(KnownControllers(), ControllersDisabledByDefault.List())); len(err) > 0 {
+		return kerrors.NewAggregate(err)
+	}
+	return nil
+}
+
+// applyFlags stores the provided arguments onto a flag set, reporting any errors
+// encountered during the process.
+func applyFlags(args map[string][]string, flags flag.NamedFlagSets) []error {
+	var errs []error
+	for key, value := range args {
+		found := false
+		// since flags are now groupped into sets we need to check each and every
+		// single of them and complain only if we don't find the flag at all
+		for _, fs := range flags.FlagSets {
+			if flag := fs.Lookup(key); flag != nil {
+				for _, s := range value {
+					if err := flag.Value.Set(s); err != nil {
+						errs = append(errs, field.Invalid(field.NewPath(key), s, fmt.Sprintf("could not be set: %v", err)))
+						break
+					}
+				}
+				found = true
+			}
+		}
+		if !found {
+			errs = append(errs, field.Invalid(field.NewPath("flag"), key, "is not a valid flag"))
+		}
+	}
+	return errs
+}

cmd/kube-controller-manager/app/patch_gc.go
@@ -1,0 +1,46 @@
+package app
+
+import (
+	gcconfig "k8s.io/kubernetes/pkg/controller/garbagecollector/config"
+
+	"k8s.io/kubernetes/cmd/kube-controller-manager/app/config"
+)
+
+func applyOpenShiftGCConfig(controllerManager *config.Config) error {
+	// TODO make this configurable or discoverable.  This is going to prevent us from running the stock GC controller
+	// IF YOU ADD ANYTHING TO THIS LIST, MAKE SURE THAT YOU UPDATE THEIR STRATEGIES TO PREVENT GC FINALIZERS
+	controllerManager.ComponentConfig.GarbageCollectorController.GCIgnoredResources = append(controllerManager.ComponentConfig.GarbageCollectorController.GCIgnoredResources,
+		// explicitly disabled from GC for now - not enough value to track them
+		gcconfig.GroupResource{Group: "authorization.openshift.io", Resource: "rolebindingrestrictions"},
+		gcconfig.GroupResource{Group: "network.openshift.io", Resource: "clusternetworks"},
+		gcconfig.GroupResource{Group: "network.openshift.io", Resource: "egressnetworkpolicies"},
+		gcconfig.GroupResource{Group: "network.openshift.io", Resource: "hostsubnets"},
+		gcconfig.GroupResource{Group: "network.openshift.io", Resource: "netnamespaces"},
+		gcconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthclientauthorizations"},
+		gcconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthclients"},
+		gcconfig.GroupResource{Group: "quota.openshift.io", Resource: "clusterresourcequotas"},
+		gcconfig.GroupResource{Group: "user.openshift.io", Resource: "groups"},
+		gcconfig.GroupResource{Group: "user.openshift.io", Resource: "identities"},
+		gcconfig.GroupResource{Group: "user.openshift.io", Resource: "users"},
+		gcconfig.GroupResource{Group: "image.openshift.io", Resource: "images"},
+
+		// virtual resource
+		gcconfig.GroupResource{Group: "project.openshift.io", Resource: "projects"},
+		// virtual and unwatchable resource, surfaced via rbac.authorization.k8s.io objects
+		gcconfig.GroupResource{Group: "authorization.openshift.io", Resource: "clusterroles"},
+		gcconfig.GroupResource{Group: "authorization.openshift.io", Resource: "clusterrolebindings"},
+		gcconfig.GroupResource{Group: "authorization.openshift.io", Resource: "roles"},
+		gcconfig.GroupResource{Group: "authorization.openshift.io", Resource: "rolebindings"},
+		// these resources contain security information in their names, and we don't need to track them
+		gcconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthaccesstokens"},
+		gcconfig.GroupResource{Group: "oauth.openshift.io", Resource: "oauthauthorizetokens"},
+		// exposed already as extensions v1beta1 by other controllers
+		gcconfig.GroupResource{Group: "apps", Resource: "deployments"},
+		// exposed as autoscaling v1
+		gcconfig.GroupResource{Group: "extensions", Resource: "horizontalpodautoscalers"},
+		// exposed as security.openshift.io v1
+		gcconfig.GroupResource{Group: "", Resource: "securitycontextconstraints"},
+	)
+
+	return nil
+}

pkg/controller/daemon/daemon_controller.go
@@ -119,7 +119,13 @@ type DaemonSetsController struct {
 	nodeLister corelisters.NodeLister
 	// nodeStoreSynced returns true if the node store has been synced at least once.
 	// Added as a member to the struct to allow injection for testing.
-	nodeStoreSynced cache.InformerSynced
+	nodeStoreSynced                    cache.InformerSynced
+	namespaceLister                    corelisters.NamespaceLister
+	namespaceStoreSynced               cache.InformerSynced
+	openshiftDefaultNodeSelectorString string
+	openshiftDefaultNodeSelector       labels.Selector
+	kubeDefaultNodeSelectorString      string
+	kubeDefaultNodeSelector            labels.Selector
 
 	// DaemonSet keys that need to be synced.
 	queue workqueue.RateLimitingInterface
@@ -257,6 +263,11 @@ func (dsc *DaemonSetsController) Run(workers int, stopCh <-chan struct{}) {
 
 	if !cache.WaitForNamedCacheSync("daemon sets", stopCh, dsc.podStoreSynced, dsc.nodeStoreSynced, dsc.historyStoreSynced, dsc.dsStoreSynced) {
 		return
+	}
+	if dsc.namespaceStoreSynced != nil {
+		if !cache.WaitForNamedCacheSync("daemon sets", stopCh, dsc.namespaceStoreSynced) {
+			return
+		}
 	}
 
 	for i := 0; i < workers; i++ {
@@ -1288,6 +1299,14 @@ func (dsc *DaemonSetsController) nodeShouldRunDaemonPod(node *v1.Node, ds *apps.
 			}
 		}
 	}
+
+	if matches, matchErr := dsc.namespaceNodeSelectorMatches(node, ds); matchErr != nil {
+		return false, false, false, matchErr
+	} else if !matches {
+		shouldSchedule = false
+		shouldContinueRunning = false
+	}
+
 	return
 }
 